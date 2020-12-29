011c95770fcb1c8e9f23743ea491d46042d3cec1
KoheiAsano <kk.asano.luxy@gmail.com>
2020-12-29 15:54:33 +0900

    move Var and Par to ring

--- multi-var_poly/src/ring.rs
+++ multi-var_poly/src/ring.rs
@@ -1,6 +1,6 @@
 #[derive(Eq, PartialEq, PartialOrd, Ord, Clone, Copy, Hash)]
 pub struct Var {
-    sym: char,
+    pub sym: char,
 }
 impl Var {
     pub fn new(c: char) -> Var {
@@ -14,6 +14,25 @@ impl std::fmt::Debug for Var {
     }
 }
 
+#[derive(Eq, PartialEq, PartialOrd, Ord, Clone, Copy, Hash)]
+pub struct Par {
+    pub sym: char,
+}
+
+impl Par {
+    pub fn new(c: char) -> Par {
+        Par { sym: c }
+    }
+}
+
+impl std::fmt::Debug for Par {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.sym)
+    }
+}
+
+#[derive(Eq, PartialEq, Clone)]
 pub struct Ring {
     vars: Vec<Var>,
+    pars: Vec<Par>,
 }
